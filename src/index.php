6698409383ce89112a41cdade67c7dfe1b73597d
Hendrik Volschenk <righteous.trespasser@gmail.com>
2017-06-08 18:01:31 +0200

    fix: use close to use array

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
   namespace Hvolschenk\Utilities\Arrays;
 
   function itemByKey(array $array): callable {
-    return function(string $key) {
+    return function(string $key) use ($array) {
       return $array[$key] ?? null;
     };
   }
